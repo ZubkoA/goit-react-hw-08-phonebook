304cc9b6e6fc9d997f05325e06d0951af26bf9de
ZubkoA <114995045+ZubkoA@users.noreply.github.com>
2023-06-25 20:49:41 +0300

    create path

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router';
-import { toast } from 'react-hot-toast';
+// import { toast } from 'react-hot-toast';
 import { loginThunk } from 'redux/auth/thunk';
 
 const LoginForm = () => {
